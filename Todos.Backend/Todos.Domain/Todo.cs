24ec672774827cd75d5f303e17d757de2748a51f
LepJak <evgeny.kuprienko2003@yandex.ru>
2021-11-04 20:55:36 +0300

    Add initial structure.

---
+++ Todos.Backend/Todos.Domain/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Todos.Domain
+{
+    class Todo
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool isCompleted { get; set; }
+        public DateTime ReminderDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
